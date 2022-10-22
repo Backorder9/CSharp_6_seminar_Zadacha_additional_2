e736df8fae54cc2fb4f4479e91dc987859a86bcf
Dmitry Osipovich <dosipovich@hotmail.com>
2022-10-22 23:51:13 +0300

    Основание степени преобразовано из целого в вещественное число.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 а используя рекуррентное соотношение an=a⋅an-1.
 Решение оформите в виде функции power(a, n). */
 
-int Power(int a, int n)
+double Power(double a, int n)
 {
 if (n == 1) return a;
     return a * Power(a, --n);
@@ -11,11 +11,11 @@ if (n == 1) return a;
 
 Console.WriteLine();
 Console.Write("Введите основание степени a: ");
-int a = int.Parse(Console.ReadLine());
+double a = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine();
 Console.Write("Введите показатель степени b: ");
 int n = int.Parse(Console.ReadLine());
 Console.WriteLine();
-int c = Power(a, n);
+double c = Power(a, n);
 Console.WriteLine($"{a} в степени {n} = {c}.");
 Console.WriteLine();
